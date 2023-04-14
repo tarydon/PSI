fdd9b5cd47e75b636418d0f7c7bbaf03e735c5d5
ravi <ravin@metamation.com>
2023-04-14 14:32:43 +0530

    Implemented A.18.Q1 & A.18.Q2

---
+++ Ops/Expr/GenXML.cs
@@ -0,0 +1,22 @@
+﻿namespace PSI.Ops;
+using System.Xml.Linq;
+
+public class ExprXML : Visitor<XElement> {
+   public override XElement Visit (NLiteral lit) => new ("Literal", new XAttribute ("Value", lit.Value), new XAttribute ("Type", lit.Type));
+
+   public override XElement Visit (NIdentifier ident) => new ("Ident", new XAttribute ("Name", ident.Name), new XAttribute ("Type", ident.Type));
+
+   public override XElement Visit (NUnary unary) {
+      var child = unary.Expr.Accept (this);
+      XElement elem = new ("Unary", new XAttribute ("Op", unary.Op.Kind), new XAttribute ("Type", unary.Type));
+      elem.Add (child);
+      return elem;
+   }
+
+   public override XElement Visit (NBinary binary) {
+      var (e1, e2) = (binary.Left.Accept (this), binary.Right.Accept (this));
+      XElement elem = new ("Binary", new XAttribute ("Op", binary.Op.Kind), new XAttribute ("Type", binary.Type));
+      elem.Add (e1); elem.Add (e2);
+      return elem;
+   }
+}
--- Parser/Token.cs
+++ Parser/Token.cs
@@ -1,5 +1,6 @@
 namespace PSI;
 using static Token.E;
+using static Console;
 
 // Represents a PSI language Token
 public class Token {
@@ -38,9 +39,20 @@ public class Token {
    // Utility function used to echo an error to the console
    public void PrintError () {
       if (Kind != ERROR) throw new Exception ("PrintError called on a non-error token");
-      Console.ForegroundColor = ConsoleColor.Yellow;
-      Console.WriteLine ($"At line {Line}, column {Column} of {Source.FileName}: {Text}");
-      Console.ResetColor ();
+      string s = $"File: {Source.FileName}", msg = $"{s}\r\n{new string ('\u2500', s.Length)}\r\n";
+      var (idx, n, space) = (Line - 2, Source.Lines.Length, new string (" "));
+      for (int i = idx; i <= Line; i++, idx++)
+         if (idx >= 1) msg += $"{idx,4}\u2502{space}{Source.Lines[idx - 1]}\r\n";
+      Write (msg);
+      ForegroundColor = ConsoleColor.Yellow; var nSpaces = Column + 5;
+      CursorLeft = nSpaces;
+      Write ("^\r\n");
+      CursorLeft = Math.Max (0, (nSpaces - Text.Length / 2) + 1);
+      WriteLine (Text);
+      ResetColor (); msg = "";
+      for (int i = 0; i < 2 && idx <= n; i++, idx++) 
+         msg += $"{idx,4}\u2502{space}{Source.Lines[idx - 1]}\r\n";
+      Write (msg);
    }
 
    // Helper used by the parser (maps operator sequences to E values)
